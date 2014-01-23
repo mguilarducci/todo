57d8ba658860017c175acab62c3058708f595384
Matheus Guilarducci <mguilarducci@gmail.com>
2014-01-23 19:19:44 -0200

    tab do sublime

--- src/app-todos.js
+++ src/app-todos.js
@@ -1,71 +1,71 @@
 if (typeof todoApp == "undefined")
-	todoApp = {
-		Models: {},
-		Views: {},
-		Collections: {}
-	};
+    todoApp = {
+        Models: {},
+        Views: {},
+        Collections: {}
+    };
 
 todoApp.Models.Todo = Backbone.Model.extend({
-	defaults: {
-		title: "",
-		complete: false
-	},
+    defaults: {
+        title: "",
+        complete: false
+    },
 
-	initialize: function() {
-		this.on('change', function() {
-			this.save();
-		});
-	},
+    initialize: function() {
+        this.on('change', function() {
+            this.save();
+        });
+    },
 
-	toggleStatus: function() {
-		this.set('complete', !this.get('complete'));
-	}
+    toggleStatus: function() {
+        this.set('complete', !this.get('complete'));
+    }
 });
 
 todoApp.Views.TodoItem = Backbone.View.extend({
-	tagName: 'li',
-	template: _.template(
-		"<label>" +
-			"<input type='checkbox' <% if (complete) print('checked') %> />" +
-			"<%= title %>" +
-		"</label>"
-	),
+    tagName: 'li',
+    template: _.template(
+        "<label>" +
+            "<input type='checkbox' <% if (complete) print('checked') %> />" +
+            "<%= title %>" +
+        "</label>"
+    ),
 
-	events: {
-		'click input': 'toggleStatus'
-	},
+    events: {
+        'click input': 'toggleStatus'
+    },
 
-	render: function() {
-		this.$el.html(this.template(this.model.attributes));
-		return this;
-	},
+    render: function() {
+        this.$el.html(this.template(this.model.attributes));
+        return this;
+    },
 
-	toggleStatus: function() {
-		this.model.toggleStatus();
-	}
+    toggleStatus: function() {
+        this.model.toggleStatus();
+    }
 });
 
 todoApp.Views.TodoList = Backbone.View.extend({
-	tagName: 'ul',
-	initialize: function() {
-		this.collection.on('add', this.add, this);
-	},
+    tagName: 'ul',
+    initialize: function() {
+        this.collection.on('add', this.add, this);
+    },
 
-	render: function() {
-		this.addAll();
-		return this;
-	},
+    render: function() {
+        this.addAll();
+        return this;
+    },
 
-	addAll: function() {
-		this.collection.each(this.add, this);
-	},
-	add: function(todo) {
-		var item = new todoApp.Views.TodoItem({model: todo});
-		this.$el.append(item.render().el);
-	}
+    addAll: function() {
+        this.collection.each(this.add, this);
+    },
+    add: function(todo) {
+        var item = new todoApp.Views.TodoItem({model: todo});
+        this.$el.append(item.render().el);
+    }
 });
 
 todoApp.Collections.Todos = Backbone.Collection.extend({
-	model: todoApp.Models.Todo,
-	url: 'api/todos'
+    model: todoApp.Models.Todo,
+    url: 'api/todos'
 });
--- test/app-todos-test.js
+++ test/app-todos-test.js
@@ -1,140 +1,140 @@
 var should = chai.should();
 
 describe('Application', function() {
-	it('creates a global variable for the namespace', function() {
-		should.exist(todoApp);
-	});
+    it('creates a global variable for the namespace', function() {
+        should.exist(todoApp);
+    });
 
-	it('create a global variable for Models', function() {
-		should.exist(todoApp.Models);
-	});
+    it('create a global variable for Models', function() {
+        should.exist(todoApp.Models);
+    });
 
-	it('create a global variable for Views', function() {
-		should.exist(todoApp.Views);
-	});
+    it('create a global variable for Views', function() {
+        should.exist(todoApp.Views);
+    });
 
-	it('create a global variable for Collections', function() {
-		should.exist(todoApp.Collections);
-	});
+    it('create a global variable for Collections', function() {
+        should.exist(todoApp.Collections);
+    });
 });
 
 describe('Todo Model', function() {
-	describe('Initialization', function() {
-		beforeEach(function() {
-			this.todo = new todoApp.Models.Todo();
-		});
+    describe('Initialization', function() {
+        beforeEach(function() {
+            this.todo = new todoApp.Models.Todo();
+        });
 
-		it('default status should be pending', function() {
-			this.todo.get('complete').should.be.false;
-		});
+        it('default status should be pending', function() {
+            this.todo.get('complete').should.be.false;
+        });
 
-		it('default title should be empty', function() {
-			this.todo.get('title').should.equal('');
-		});
-	});
+        it('default title should be empty', function() {
+            this.todo.get('title').should.equal('');
+        });
+    });
 });
 
 describe('Persistence', function() {
-	beforeEach(function() {
-		this.todo = new todoApp.Models.Todo();
-		this.save_stub = sinon.stub(this.todo, "save");
-	});
+    beforeEach(function() {
+        this.todo = new todoApp.Models.Todo();
+        this.save_stub = sinon.stub(this.todo, "save");
+    });
 
-	afterEach(function() {
-		this.save_stub.restore();
-	});
+    afterEach(function() {
+        this.save_stub.restore();
+    });
 
-	it('should update server when title is changed', function() {
-		this.todo.set('title', 'new title');
-		this.save_stub.should.have.been.calledOnce;
-	});
+    it('should update server when title is changed', function() {
+        this.todo.set('title', 'new title');
+        this.save_stub.should.have.been.calledOnce;
+    });
 
-	it('should update server when status is changed', function() {
-		this.todo.set('complete', true);
-		this.save_stub.should.have.been.calledOnce;
-	});
+    it('should update server when status is changed', function() {
+        this.todo.set('complete', true);
+        this.save_stub.should.have.been.calledOnce;
+    });
 });
 
 describe('Todo item view', function() {
-	beforeEach(function() {
-		this.todo = new todoApp.Models.Todo({title: 'new title'});
-		this.item = new todoApp.Views.TodoItem({model: this.todo});
-		this.save_stub = sinon.stub(this.todo, 'save');
-	});
+    beforeEach(function() {
+        this.todo = new todoApp.Models.Todo({title: 'new title'});
+        this.item = new todoApp.Views.TodoItem({model: this.todo});
+        this.save_stub = sinon.stub(this.todo, 'save');
+    });
 
-	afterEach(function() {
-		this.save_stub.restore();
-	});
+    afterEach(function() {
+        this.save_stub.restore();
+    });
 
-	it('render() should return the view object', function() {
-		this.item.render().should.equal(this.item);
-	});
+    it('render() should return the view object', function() {
+        this.item.render().should.equal(this.item);
+    });
 
-	it('should render <li> tag', function() {
-		this.item.render().el.nodeName.should.equal('LI');
-	});
+    it('should render <li> tag', function() {
+        this.item.render().el.nodeName.should.equal('LI');
+    });
 
-	describe('Template', function() {
-		beforeEach(function() {
-			this.item.render();
-		});
+    describe('Template', function() {
+        beforeEach(function() {
+            this.item.render();
+        });
 
-		it('should contain the todo title as a text', function() {
-			this.item.$el.text().should.have.string('new title');
-		});
+        it('should contain the todo title as a text', function() {
+            this.item.$el.text().should.have.string('new title');
+        });
 
-		it('should include a label', function() {
-			this.item.$el.find('label').should.have.length(1);
-		});
+        it('should include a label', function() {
+            this.item.$el.find('label').should.have.length(1);
+        });
 
-		it('should include a checkbox', function() {
-			this.item.$el.find("label>input[type='checkbox']").should.have.length(1);
-		});
+        it('should include a checkbox', function() {
+            this.item.$el.find("label>input[type='checkbox']").should.have.length(1);
+        });
 
-		it('checkbox should be clear as default', function() {
-			this.item.$el.find("label>input[type='checkbox']").is(':checked').should.be.false;
-		});
+        it('checkbox should be clear as default', function() {
+            this.item.$el.find("label>input[type='checkbox']").is(':checked').should.be.false;
+        });
 
-		it('checkbox should be set for "complete" todos', function() {	
-			this.todo.set('complete', true);
-			this.item.render();
-			this.item.$el.find("label>input[type='checkbox']").is(":checked").should.be.true;
-		});
-	});
+        it('checkbox should be set for "complete" todos', function() {    
+            this.todo.set('complete', true);
+            this.item.render();
+            this.item.$el.find("label>input[type='checkbox']").is(":checked").should.be.true;
+        });
+    });
 
-	describe('Model Interaction', function() {
-		it('should update model when the chackbox is clicked', function() {
-			$('<div>').attr('id', 'fixture').css('display', 'none').appendTo('body');
-			this.item.render();
-			$('#fixture').append(this.item.$el);
-			this.item.$el.find('input').click();
-			this.todo.get('complete').should.be.true;
-			$('#fixture').remove();
-		});
-	});
+    describe('Model Interaction', function() {
+        it('should update model when the chackbox is clicked', function() {
+            $('<div>').attr('id', 'fixture').css('display', 'none').appendTo('body');
+            this.item.render();
+            $('#fixture').append(this.item.$el);
+            this.item.$el.find('input').click();
+            this.todo.get('complete').should.be.true;
+            $('#fixture').remove();
+        });
+    });
 });
 
 describe('Todo List View', function() {
-	beforeEach(function() {
-		this.todos = new todoApp.Collections.Todos([
-			{title: 'title 1'},
-			{title: 'title 2'}
-		]);
+    beforeEach(function() {
+        this.todos = new todoApp.Collections.Todos([
+            {title: 'title 1'},
+            {title: 'title 2'}
+        ]);
 
-		this.list = new todoApp.Views.TodoList({collection: this.todos});
-	});
+        this.list = new todoApp.Views.TodoList({collection: this.todos});
+    });
 
-	it('render() should return the view object', function() {
-		this.list.render().should.equal(this.list);
-	});
+    it('render() should return the view object', function() {
+        this.list.render().should.equal(this.list);
+    });
 
-	it('should render <ul> tag', function() {
-		this.list.render().el.nodeName.should.equal('UL');
-	});
+    it('should render <ul> tag', function() {
+        this.list.render().el.nodeName.should.equal('UL');
+    });
 
-	it('should include list items for all models in collection', function() {
-		this.list.render();
-		this.list.$el.find('li').should.have.length(2);
-	});
+    it('should include list items for all models in collection', function() {
+        this.list.render();
+        this.list.$el.find('li').should.have.length(2);
+    });
 
 });
